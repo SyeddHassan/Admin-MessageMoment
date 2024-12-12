3473fe710a097b5618a4ca00c6d9ff02a91ca0d4
SyeddHassan <syeddhassanali@gmail.com>
2024-12-12 02:04:51 -0800

    added a prototype interface of dashboard users tab

--- src/components/maps/real-time-sessions-map.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import React, { useLayoutEffect, useRef } from "react";
-
-import * as am5 from "@amcharts/amcharts5";
-import * as am5map from "@amcharts/amcharts5/map";
-import am5geodataWorldLow from "@amcharts/amcharts5-geodata/worldLow";
-import am5themes_Animated from "@amcharts/amcharts5/themes/Animated";
